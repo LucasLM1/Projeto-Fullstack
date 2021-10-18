c9288364f7e7ee0004363b536009677370531270
LucasLM1 <lucascorreia325@gmail.com>
2021-10-18 15:41:20 -0300

    Alterações no Back-end

--- projeto/src/main/java/com/lucaslm/projeto/service/DBService.java
+++ projeto/src/main/java/com/lucaslm/projeto/service/DBService.java
@@ -24,12 +24,12 @@ public class DBService {
 	public void InstanciaBaseDeDados() throws ParseException {
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 
-		Todo t1 = new Todo(null, "Estudar", "Estudar Spring Boot 2 e Angular 11", sdf.parse("25/03/2022"), true);
-		Todo t2 = new Todo(null, "Ler", "Ler livro de desenvolvimento pessoal", sdf.parse("22/03/2021"), true);
-		Todo t3 = new Todo(null, "Exercicios", "Praticar exercicios fisicos", sdf.parse("21/03/2022 13:40"), false);
-		Todo t4 = new Todo(null, "Meditar", "Meditar durante 30 minutos pela manhã", sdf.parse("27/03/2021"), true);
-		Todo t5 = new Todo(null, "Correr", "Correr 20 kilometros ", sdf.parse("27/09/2021"), true);
-		Todo t6 = new Todo(null, "Trabalho", "Trabalho da faculdade para apresentar", sdf.parse("27/05/2021"), false);
+		Todo t1 = new Todo(null, "Estudar", "Estudar Spring Boot e Angular 12", sdf.parse("06/10/2022"), true);
+		Todo t2 = new Todo(null, "Ler", "Terminar o livro a arte da guerra", sdf.parse("31/12/2021"), false);
+		Todo t3 = new Todo(null, "Ranking", "Subir para o Ranking Grão Mestre", sdf.parse("30/10/2021"), false);
+		Todo t4 = new Todo(null, "Projeto", "Terminar o projeto fullstack/ Lista de tarefas", sdf.parse("20/10/2021"), false);
+		Todo t5 = new Todo(null, "Caminhar", "Caminhar 10 kilometros ", sdf.parse("18/10/2021"), true);
+		Todo t6 = new Todo(null, "Atividade", "Terminar atividades da faculdade", sdf.parse("18/10/2021"), true);
 	    
 
 		todoRepository.saveAll(Arrays.asList(t1, t2, t3, t4, t5, t6));
